470b0ce1b9fd468ce4715ff41e226acd55c7b4b2
vitalyMatveev <vvital_2000@mail.ru>
2021-08-13 17:07:52 +0300

    подключил jquery и normalize к проекту

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,16 @@
-console.log('script work!');
+
+$(document).ready(function () {
+  // скрипт jquery всегда пишем в этих фигурных скобках
+  // $ - эй jquery
+  console.log('script work!');
+
+
+  // $('.services-item .services-head').css('color', 'red');
+
+  // let $headText = $('.services-item .services-head');
+
+  // $headText.each(function (index, element) {
+  //   console.log($(element).text());
+  // });
+
+});
